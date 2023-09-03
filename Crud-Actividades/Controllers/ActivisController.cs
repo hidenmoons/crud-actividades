233fa3ffbbbf09712a5176c72c68ea56d4368ef0
Joan Luna <alexis_dx8@hotmail.com>
2023-09-03 16:18:26 -0700

    agregar actividades

--- Crud-Actividades/Controllers/ActivisController.cs
+++ Crud-Actividades/Controllers/ActivisController.cs
@@ -24,5 +24,38 @@ namespace Crud_Actividades.Controllers
             return StatusCode(StatusCodes.Status200OK, propiedades);
 
         }
+
+        [HttpPost]
+        public async Task<IActionResult> AgregarActividad(ActivityDTO Actividad)
+        {
+            int id = Actividad.PropertyId;
+
+            var find_propiedad = await _actividadesContext.Properties.FindAsync(id);
+
+            if (find_propiedad == null)
+            {
+                return StatusCode(StatusCodes.Status400BadRequest,"Propiedad no encontrada");
+            }
+            else
+            {
+                var nueva_actividad = new Activity
+                {
+                    CreatedAt = DateTime.UtcNow,
+                    PropertyId = Actividad.PropertyId,
+                    Status = Actividad.Status,
+                    Title = Actividad.Title,
+                    UpdatedAt = DateTime.UtcNow,
+                    Schedule = Actividad.Schedule,
+                };
+
+                await _actividadesContext.Activities.AddAsync(nueva_actividad);
+                await _actividadesContext.SaveChangesAsync();
+                return StatusCode(StatusCodes.Status200OK, nueva_actividad);
+            }
+
+         
+
+          
+        }
     }
 }
--- Crud-Actividades/Models/ActivityDTO.cs
+++ Crud-Actividades/Models/ActivityDTO.cs
@@ -2,5 +2,12 @@
 {
     public class ActivityDTO
     {
+        public int PropertyId { get; set; }
+
+        public DateTime Schedule { get; set; }
+
+        public string Title { get; set; } = null!;
+
+        public string Status { get; set; } = null!;
     }
 }

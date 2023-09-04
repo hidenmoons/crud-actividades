519d21f2d26f92da288561931acc88269ae8ffcb
Joan Luna <alexis_dx8@hotmail.com>
2023-09-04 04:10:36 -0700

    query propiedades

---
+++ Crud-Actividades/Controllers/PropertysController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Crud_Actividades.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Crud_Actividades.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PropertysController : ControllerBase
+    {
+        readonly DbActividadesContext _actividadesContext;
+
+        public PropertysController(DbActividadesContext _actividadesContext) 
+        {
+           this._actividadesContext = _actividadesContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Getpropietys()
+        {
+            var propiedades = _actividadesContext.Properties.AsNoTracking();
+
+            return StatusCode(StatusCodes.Status200OK, propiedades);
+
+        }
+    }
+}
---
+++ propeidadesquery.sql
@@ -0,0 +1,22 @@
+INSERT INTO Property (tittle, address, descripcion, created_at, update_at, disable_at, status)
+VALUES
+('Casa en la Playa', '123 Calle de la Playa', 'Amplia casa frente al mar', GETDATE(), GETDATE(), NULL, 'ACTIVA'),
+('Apartamento en la Ciudad', '456 Avenida Principal', 'Moderno apartamento céntrico', GETDATE(), GETDATE(), NULL, 'ACTIVA'),
+('Casa en las Montañas', '789 Camino de Montaña', 'Acogedora casa de montaña', GETDATE(), GETDATE(), NULL, 'ACTIVA'),
+('Piso Ejecutivo', '101 Torre Ejecutiva', 'Lujoso piso ejecutivo', GETDATE(), GETDATE(), NULL, 'ACTIVA'),
+('Terreno para Desarrollo', '222 Calle del Desarrollo', 'Terreno ideal para desarrollo', GETDATE(), GETDATE(), NULL, 'ACTIVA'),
+('Apartamento Estudiantil', '333 Residencia Universitaria', 'Apartamento para estudiantes', GETDATE(), GETDATE(), NULL, 'ACTIVA'),
+('Casa de Campo', '444 Camino Rural', 'Encantadora casa de campo', GETDATE(), GETDATE(), NULL, 'ACTIVA'),
+('Condominio en la Ciudad', '555 Condominio Urbano', 'Condominio en el centro de la ciudad', GETDATE(), GETDATE(), NULL, 'ACTIVA'),
+('Parcela en la Playa', '666 Parcela Costera', 'Terreno en la costa', GETDATE(), GETDATE(), NULL, 'ACTIVA'),
+('Apartamento con Vistas', '777 Avenida Panorámica', 'Apartamento con vistas panorámicas', GETDATE(), GETDATE(), NULL, 'ACTIVA');
+('Casa en el Bosque', '123 Camino del Bosque', 'Casa en medio del bosque', GETDATE(), GETDATE(), NULL, 'INACTIVA'),
+('Apartamento en el Centro', '456 Avenida del Centro', 'Apartamento céntrico antiguo', GETDATE(), GETDATE(), NULL, 'INACTIVA'),
+('Terreno en Suburbio', '789 Suburbio Sereno', 'Terreno en área suburbana', GETDATE(), GETDATE(), NULL, 'INACTIVA'),
+('Piso de Oficinas', '101 Edificio de Oficinas', 'Piso de oficinas corporativas', GETDATE(), GETDATE(), NULL, 'INACTIVA'),
+('Terreno Industrial', '222 Zona Industrial', 'Terreno para desarrollo industrial', GETDATE(), GETDATE(), NULL, 'INACTIVA'),
+('Apartamento Vacacional', '333 Costa Vacacional', 'Apartamento de alquiler vacacional', GETDATE(), GETDATE(), NULL, 'INACTIVA'),
+('Casa Histórica', '444 Calle Histórica', 'Casa histórica restaurada', GETDATE(), GETDATE(), NULL, 'INACTIVA'),
+('Terreno en el Campo', '555 Terreno Rural', 'Terreno en zona rural', GETDATE(), GETDATE(), NULL, 'INACTIVA'),
+('Apartamento de Inversión', '666 Avenida de Inversión', 'Apartamento para inversión', GETDATE(), GETDATE(), NULL, 'INACTIVA'),
+('Condominio de Lujo', '777 Condominio de Lujo', 'Condominio de lujo en la playa', GETDATE(), GETDATE(), NULL, 'INACTIVA');
